ac8abb723bec0e6cfbe7b486d0120a19aa4dd143
TheKodeToad <TheKodeToad@proton.me>
2023-03-14 00:02:15 +0000

    Feature?

---
+++ src/main/java/io/toadlabs/toadhud/ToadHudMod.java
@@ -0,0 +1,17 @@
+package io.toadlabs.toadhud;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ToadHudMod implements ModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("ToadHud");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+
+}
